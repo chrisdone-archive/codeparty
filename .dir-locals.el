ed94e7b22d4fbe295c1edbd47fcff001d4ff3162
Chris Done <chrisdone@gmail.com>
2019-02-17 17:32:35 +0000

    Update targets

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((haskell-mode
-  (intero-targets "codeparty:lib" "codeparty:exe:codeparty")))
+  (intero-targets "codeparty:lib" "codeparty:exe:codeparty" "codeparty:exe:codeparty-server")))
